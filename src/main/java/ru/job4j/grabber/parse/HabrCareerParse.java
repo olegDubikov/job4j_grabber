93eaca1ae600d84c013c35b3fbc79ebe5f6e92fe
олегДубиков <lars75@inbox.ru>
2024-04-13 11:32:10 +0300

    +

--- src/main/java/ru/job4j/grabber/parse/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/parse/HabrCareerParse.java
@@ -17,7 +17,7 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
-
+    private static final int PAGE = 5;
     private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
@@ -28,7 +28,7 @@ public class HabrCareerParse implements Parse {
     public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
         int pageNumber = 1;
-        while (pageNumber <= 5) {
+        while (pageNumber <= PAGE) {
             try {
                 String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
                 Connection connection = Jsoup.connect(fullLink);
